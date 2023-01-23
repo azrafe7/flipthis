57683e5f01441f3a571ebaab3928248eea0d4ceb
azrafe7 <azrafe7@gmail.com>
2023-01-23 01:04:03 +0100

    flip this v0.3.1 (update 2022/2023)

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -13,49 +13,51 @@ function getUniqueId() {
   function id4() {
     return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
   }
-  
+
   return id4()+id4();
 }
 
 function flip(flipState, wholePage, targetUID) {
-	var currFlipState = getFlipState(wholePage ? null : currTarget);
-	var newState = flipState;
-  
+  var currFlipState = getFlipState(wholePage ? null : currTarget);
+  var newState = flipState;
+
   // skip if requested to flip page but not in top frame
   // or requested to flip element but not the right content script
-  if ((!wholePage && targetUID != uid) || (wholePage && window.top !== window)) {  
+  if ((!wholePage && targetUID != uid) || (wholePage && window.top !== window)) {
     //console.log(["skip!", "page", wholePage, "win", window.top === window, uid, targetUID]);
-    return; 
+    return;
   }
   //console.log(["flipState current vs new:", currFlipState, newState]);
-	setFlipState(wholePage ? null : currTarget, newState);
-	if (wholePage) {
-		chrome.extension.sendRequest({command:"getFlipState", flipState:newState});
-	}
+  setFlipState(wholePage ? null : currTarget, newState);
+  if (wholePage) {
+    chrome.extension.sendRequest({command:"getFlipState", flipState:newState});
+  }
 }
 
 function setFlipState(selector, state) {
   var $selector = $(selector || "body");
-	var newState = state || defaultFlipState;
-  if (selector && (selector.tagName == "SPAN" || selector.tagName == "A")) $selector = $selector.parent();
-	$selector.toggleClass("flipthis-animate", settings.animate);
-	$selector.toggleClass("flipthis-invisible", settings.animate);
-	$selector.css("-webkit-transform", 
-		(newState.flipX ? "scaleX(-1) " : "") + 
-		(newState.flipY ? "scaleY(-1) " : "") + 
-		(newState.rotate != 0 ? "rotate(" + (newState.rotate != 270 ? newState.rotate : -90) + "deg) " : ""));
-	//console.log([$selector[0], newState.rotate, "rotate(" + (newState.rotate != 270 ? newState.rotate : -90) + "deg) "]);
-	$selector.removeClass("flipthis-invisible");
+  var newState = state || defaultFlipState;
+  // get wrapping DIV of specified elements (was needed in the past to make them transform properly)
+  //if (selector && (selector.tagName == "SPAN" || selector.tagName == "A")) $selector = $selector.parent();
+  $selector.toggleClass("flipthis-animate", settings.animate);
+  $selector.toggleClass("flipthis-invisible", settings.animate);
+  $selector.css("-webkit-transform",
+    (newState.flipX ? "scaleX(-1) " : "") +
+    (newState.flipY ? "scaleY(-1) " : "") +
+    (newState.rotate != 0 ? "rotate(" + (newState.rotate != 270 ? newState.rotate : -90) + "deg) " : ""));
+  //console.log([$selector[0], newState.rotate, "rotate(" + (newState.rotate != 270 ? newState.rotate : -90) + "deg) "]);
+  $selector.removeClass("flipthis-invisible");
 }
 
 function getFlipState(selector) {
-	var state = {flipX:false, flipY:false, rotate:0};
-	var $selector = $(selector || "body");
+  var state = {flipX:false, flipY:false, rotate:0};
+  var $selector = $(selector || "body");
   if ($selector.length) {
-    if (selector && (selector.tagName == "SPAN" || selector.tagName == "A") && !$selector.parent().hasClass("flipthis-wrapper")) {
-      $selector = $selector.wrap("<div class='flipthis-wrapper'>").parent();
-      //console.log(["wrapped", $selector]);
-    }
+    // wrap specified elements into a DIV (was needed in the past to make them transform properly)
+    //if (selector && (selector.tagName == "SPAN" || selector.tagName == "A") && !$selector.parent().hasClass("flipthis-wrapper")) {
+    //  $selector = $selector.wrap("<div class='flipthis-wrapper'>").parent();
+    //  //console.log(["wrapped", $selector]);
+    //}
     var transform = $selector[0].style.webkitTransform;
     state.flipX = transform.indexOf("scaleX(-1)") >= 0;
     state.flipY = transform.indexOf("scaleY(-1)") >= 0;
@@ -66,76 +68,76 @@ function getFlipState(selector) {
     }
     //console.log(["webkitTransform:", $selector[0].style.webkitTransform]);
   }
-	return state;
+  return state;
 }
 
 function processSettings(JSONsettings) {
-	settings = JSON.parse(JSONsettings);
-	enabled = location.href.match(new RegExp(settings.urlPattern, "i"));
-	//console.log(["URL Matching:", location.href, settings.urlPattern, enabled, !!enabled]);
-	chrome.extension.sendRequest({command:"enable", enabled:!!enabled});
-	if (enabled && settings.autoFlip && firstRun) {
-		$(document).ready(function() {
-			//console.log("AutoFlip!");
-			flip(settings, true);
-		});
-	}
-	firstRun = false;
+  settings = JSON.parse(JSONsettings);
+  enabled = location.href.match(new RegExp(settings.urlPattern, "i"));
+  //console.log(["URL Matching:", location.href, settings.urlPattern, enabled, !!enabled]);
+  chrome.extension.sendRequest({command:"enable", enabled:!!enabled});
+  if (enabled && settings.autoFlip && firstRun) {
+    $(document).ready(function() {
+      //console.log("AutoFlip!");
+      flip(settings, true);
+    });
+  }
+  firstRun = false;
 }
 
 // initialize
 function init() {
 
-	// listen to messages sent from background and popup page
-	chrome.extension.onRequest.addListener(function (request, sender, callback) {
-		if (request.command == "flip") {
-			//console.log("received flip");
-			if (enabled) flip(request.flipState, request.wholePage, request.targetUID);
-		} else if (request.command == "settings") {
-			processSettings(request.settings);
-		} else if (request.command == "getFlipState") {
-			if (settings.contextMenu || !enabled) chrome.extension.sendRequest({command:"contextMenu", tag:null, flipState:null});
-			if (enabled) chrome.extension.sendRequest({command:"getFlipState", flipState:getFlipState()});
-		} else {
-			callback({});
-		}
-	});
+  // listen to messages sent from background and popup page
+  chrome.extension.onRequest.addListener(function (request, sender, callback) {
+    if (request.command == "flip") {
+      //console.log("received flip");
+      if (enabled) flip(request.flipState, request.wholePage, request.targetUID);
+    } else if (request.command == "settings") {
+      processSettings(request.settings);
+    } else if (request.command == "getFlipState") {
+      if (settings.contextMenu || !enabled) chrome.extension.sendRequest({command:"contextMenu", tag:null, flipState:null});
+      if (enabled) chrome.extension.sendRequest({command:"getFlipState", flipState:getFlipState()});
+    } else {
+      callback({});
+    }
+  });
 
-	// request settings
-	chrome.extension.sendRequest({command: "settingsChanged"});
-	
-	$(document).ready(function() {
-		//console.log("doc ready!");
-	});
-	
-	// catch mouse down events to send context menu items
-	document.addEventListener("mouseup", onMouseUp, false);
-	document.addEventListener("mousedown", onMouseDown, false);
-	document.addEventListener("mousemove", onMouseMove, false);
+  // request settings
+  chrome.extension.sendRequest({command: "settingsChanged"});
 
-	console.log("Flip script injected!");
+  $(document).ready(function() {
+    //console.log("doc ready!");
+  });
+
+  // catch mouse down events to send context menu items
+  document.addEventListener("mouseup", onMouseUp, false);
+  document.addEventListener("mousedown", onMouseDown, false);
+  document.addEventListener("mousemove", onMouseMove, false);
+
+  console.log("Flip script injected!");
 }
 
 /* // old highlight code
 function flashStep(target, origAlpha, down) {
-	var alpha = parseFloat($(target).css("opacity") || "1");
-	var alphaStep = origAlpha/2;
-	var newAlpha = down ? alpha-alphaStep : alpha+alphaStep;
-	if (newAlpha < 0) newAlpha = 0;
-	if (newAlpha > origAlpha) newAlpha = origAlpha;
-	$(target).css("opacity", newAlpha.toString());
-	if (newAlpha <= 0) down = false;
-	if (newAlpha < origAlpha) {
-		setTimeout(function() { flashStep(target, origAlpha, down); }, 10);
-	}
+  var alpha = parseFloat($(target).css("opacity") || "1");
+  var alphaStep = origAlpha/2;
+  var newAlpha = down ? alpha-alphaStep : alpha+alphaStep;
+  if (newAlpha < 0) newAlpha = 0;
+  if (newAlpha > origAlpha) newAlpha = origAlpha;
+  $(target).css("opacity", newAlpha.toString());
+  if (newAlpha <= 0) down = false;
+  if (newAlpha < origAlpha) {
+    setTimeout(function() { flashStep(target, origAlpha, down); }, 10);
+  }
 }
 
 function flashAnimate(target) {
-	var tgt = $(target);
+  var tgt = $(target);
   var origAlpha = parseFloat(tgt.css("opacity") || "1");
   tgt.addClass("flipthis-highlight");
-	var t = setTimeout(function() { 
-    flashStep(target, origAlpha, true); 
+  var t = setTimeout(function() {
+    flashStep(target, origAlpha, true);
   }, 10);
   setTimeout(function() { tgt.removeClass("flipthis-highlight"); }, 100);
 }*/
@@ -149,9 +151,9 @@ function sendTarget(target) {
     console.log(["Sending target flipState:", targetFlipState, target.tagName]);
     currTarget = target;
     chrome.extension.sendRequest({
-      command:"contextMenu", 
-      remove:!enabled, 
-      tag:validTarget? target.tagName : null, 
+      command:"contextMenu",
+      remove:!enabled,
+      tag:validTarget? target.tagName : null,
       flipState: JSON.stringify(targetFlipState),
       uid: uid
     });
@@ -173,24 +175,24 @@ function highlight(target) {
 
 // send curr target and highlight
 function onMouseDown(event) {
-	//console.log([event.which, event.target]);
-  if (enabled && event.which == 3 && settings.contextMenu) {		// right click
+  //console.log([event.which, event.target]);
+  if (enabled && event.which == 3 && settings.contextMenu) {    // right click
     sendTarget(event.target);
     if (settings.blink) highlight(event.target);
-	}
+  }
 }
 
 // de-highlight
 function onMouseUp(event) {
-  if (enabled && event.which == 3 && settings.blink) {		// right click
+  if (enabled && event.which == 3 && settings.blink) {    // right click
     highlight(null);
-	}
+  }
 }
 
 // send curr target and highlight
 function onMouseMove(event) {
   //console.log(["m", highlighted.length]);
-  if (enabled && event.which == 3 && settings.blink && settings.contextMenu) {		// highlight target while holding right-click
+  if (enabled && event.which == 3 && settings.blink && settings.contextMenu) {    // highlight target while holding right-click
     sendTarget(event.target);
     if (settings.blink) highlight(event.target);
   }
--- src/js/options.js
+++ src/js/options.js
@@ -1,5 +1,5 @@
 
-var bgPage = chrome.extension.getBackgroundPage();	// ref to background page object
+var bgPage = chrome.extension.getBackgroundPage();  // ref to background page object
 
 var extensionUrl = "https://chrome.google.com/extensions/search?hl=en&q=";
 var manifest = {};  // extension manifest object
@@ -9,49 +9,49 @@ var settings = {};
 
 // SettingsHelper Object
 SettingsHelper = function() {
-	return this;
+  return this;
 }
 
 SettingsHelper.prototype = {
 
-	getFromDisk: function() {
-		settings = JSON.parse(localStorage.settings);
-	},
-	
-	getFromPage: function() {
-		settings.autoFlip = $("#autoFlip").attr("checked");
-		settings.flipX = $("#flipX").attr("checked");
-		settings.flipY = $("#flipY").attr("checked");
-		settings.rotate = $("#rotate").val();
-		settings.animate = $("#animate").attr("checked");
-		settings.contextMenu = $("#contextMenu").attr("checked");
-		settings.blink = $("#blink").attr("checked");
-		settings.urlPattern = $("#urlPattern").val();
-	},
-	
-	saveToPage: function() {
-		$("#autoFlip").attr("checked", settings.autoFlip);
-		$("#flipX").attr("checked", settings.flipX);
-		$("#flipY").attr("checked", settings.flipY);
-		$("#rotate").val(settings.rotate);
-		$("#animate").attr("checked", settings.animate);
-		$("#contextMenu").attr("checked", settings.contextMenu);
-		$("#blink").attr("checked", settings.blink);
-		$("#urlPattern").val(settings.urlPattern);
-	},
-	
-	saveToDisk: function() {
-		localStorage.settings = JSON.stringify(settings);
-	},
-	
-	setDefaults: function() {
-		settings = JSON.parse(localStorage.defaults);
-	}
+  getFromDisk: function() {
+    settings = JSON.parse(localStorage.settings);
+  },
+
+  getFromPage: function() {
+    settings.autoFlip = $("#autoFlip").attr("checked");
+    settings.flipX = $("#flipX").attr("checked");
+    settings.flipY = $("#flipY").attr("checked");
+    settings.rotate = $("#rotate").val();
+    settings.animate = $("#animate").attr("checked");
+    settings.contextMenu = $("#contextMenu").attr("checked");
+    settings.blink = $("#blink").attr("checked");
+    settings.urlPattern = $("#urlPattern").val();
+  },
+
+  saveToPage: function() {
+    $("#autoFlip").attr("checked", settings.autoFlip);
+    $("#flipX").attr("checked", settings.flipX);
+    $("#flipY").attr("checked", settings.flipY);
+    $("#rotate").val(settings.rotate);
+    $("#animate").attr("checked", settings.animate);
+    $("#contextMenu").attr("checked", settings.contextMenu);
+    $("#blink").attr("checked", settings.blink);
+    $("#urlPattern").val(settings.urlPattern);
+  },
+
+  saveToDisk: function() {
+    localStorage.settings = JSON.stringify(settings);
+  },
+
+  setDefaults: function() {
+    settings = JSON.parse(localStorage.defaults);
+  }
 }
 
 
 
-init();	// initialize
+init();  // initialize
 
 // execute main() when DOM is ready
 $(document).ready(main);
@@ -59,39 +59,39 @@ $(document).ready(main);
 
 // initialize
 function init() {
-	settingsHelper = new SettingsHelper();
-	settingsHelper.getFromDisk(); 	// load settings from disk
+  settingsHelper = new SettingsHelper();
+  settingsHelper.getFromDisk();   // load settings from disk
 }
 
 // main function (executed when the DOM is ready)
 function main() {
-	// read manifest and write extension title & ver & desc
-	readManifestFile();
-	
-	settingsHelper.saveToPage();
-	
-	$("input,select").bind("change", function() { sendSettings(); });
+  // read manifest and write extension title & ver & desc
+  readManifestFile();
+
+  settingsHelper.saveToPage();
+
+  $("input,select").bind("change", function() { sendSettings(); });
 }
 
 
 function sendSettings() {
-	settingsHelper.getFromPage();
-	settingsHelper.saveToDisk();
-	chrome.extension.sendRequest({command: "settingsChanged"});
-	console.log(["Sending new settings:", settings]);
+  settingsHelper.getFromPage();
+  settingsHelper.saveToDisk();
+  chrome.extension.sendRequest({command: "settingsChanged"});
+  console.log(["Sending new settings:", settings]);
 }
 
 
 // read manifest file and put parsed object in manifest global variable
 function readManifestFile() {
-	xhr = new XMLHttpRequest();
-	xhr.open("GET", chrome.extension.getURL("manifest.json"));
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			manifest = JSON.parse(xhr.responseText);
-			$("#title").text(manifest.name + " v" + manifest.version + " - Options");
-		} 
-	}
-	xhr.send();
+  xhr = new XMLHttpRequest();
+  xhr.open("GET", chrome.extension.getURL("manifest.json"));
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      manifest = JSON.parse(xhr.responseText);
+      $("#title").text(manifest.name + " v" + manifest.version + " - Options");
+    }
+  }
+  xhr.send();
 }
 
